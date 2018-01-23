601c3a4a922ebb8caee4a7048d7201e99c90341e
elismar2016 <elismar.tecnico@gmail.com>
2018-01-22 20:45:26 -0500

    versao 1.0.0

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,7 +3,9 @@ defined('BASEPATH') OR exit('No direct');
 
 class Welcome extends CI_Controller{
     public function index(){
-        $this->load->view('public/welcome');
+        $this->load->view('public/header');
+        $this->load->view('public/menu_left');
+        $this->load->view('public/conteudo');
     }
 }
 ?>
