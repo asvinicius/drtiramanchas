f26bb9346836dc4fa6b5c19441ed546367dcb708
asvinicius <anjos.vinicius@hotmail.com>
2018-02-07 02:55:45 -0500

    Versão 1.0.1

--- application/views/public/atendimento.php
+++ application/views/public/atendimento.php
@@ -1,30 +1,27 @@
-﻿
-<?php require 'topo.php'; ?>
-<?php require 'lateral.php'; ?>
-<html>
-  <body>
-  <div class="conteudo">  
-   <div class="row">
+       <div id="page-wrapper" >
+            <div class="row">
                 <div class="col-md-12">
-                    <!-- Advanced Tables -->
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-<p class="caminho">Você esta aqui: <a rel="nofollow" href="http://www.drtiramancha.com.br/" title="Voltar a página inicial">Home</a> » <a href="http://www.drtiramancha.com.br/informacao.php" title="Voltar a página Informações ">Informações </a> » <strong>Atendimento</strong></p>	
-	
-								
-                            <strong>Abrangencia</strong>
-                        </div>
-                        <div class="panel-body">
-                            <div class="table-responsive text-justify">
-							<img class="img-rounded img-responsive"	width="600" height="600" src="assets/fotos/gsp.jpg"/>
-        	
+                     <h2>Abrangência</h2>   
+                     <h5>Solicite uma coleta ou visita para limpeza no local, ligue agora mesmo, em breve o seu agendamento sará nesse site. </h5>
 
-<p>Atendemos na grande São Paulo.</p>
-<p>Solicite uma coleta ou visita para limpeza no local, ligue agora mesmo, em breve o seu agendamento sará nesse site.</p>
-	<p><a class="btn btn-success" href="painel.php" title="Contato">Agendamento</a></p>
-		</div>	
-	</div>	 </div> </div> </div> </div>
-
-<?php require 'footer.php'; ?>
+                    </div>
+            </div>
+             <!-- /. ROW  -->
+             <hr />
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+      
+     <!-- /. WRAPPER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    <script src=<?= base_url('assets/js/jquery-1.10.2.js'); ?>></script>
+      <!-- BOOTSTRAP SCRIPTS -->
+    <script src=<?= base_url('assets/js/bootstrap.min.js'); ?>></script>
+    <!-- METISMENU SCRIPTS -->
+    <script src=<?= base_url('assets/js/jquery.metisMenu.js'); ?>></script>
+      <!-- CUSTOM SCRIPTS -->
+    <script src=<?= base_url('assets/js/custom.js'); ?>></script>
+    
+   
 </body>
 </html>
--- readme.rst
+++ readme.rst
@@ -3,7 +3,7 @@ DR TIRA MANCHAS
 ###################
 
 Fiz uma mudança em 'views/public/welcome.php' tirei a <div id="page-inner"> porque achei que estivesse perdendo espaço.
-E também tirei no menu_left o botão HOME, por onde vai ficar a logo ja vai dar caminho para home, não é necessário.
+E também tirei no menu_left o botão HOME, porque onde vai ficar a logo ja vai dar caminho para home, não é necessário.
 
 Agora vamos ao principal:
 
@@ -14,7 +14,7 @@ Ao fazer isso, o codeigniter entende que estamos buscando um controlador com nom
 --- caso vc faça isso sem ter o controlador de tal nome vai dar um erro do codeigniter dizendo que ele não existe---
 Então o próximo passo foi criar o controlador contact.
 Quando o codeigniter manda acessar o controlador, no caso no formato href="<?= base_url('contact'); ?>", ele vai buscar
-    dentro do controlador a função index por padrão.
+dentro do controlador a função index por padrão.
 Então devemos criar uma função index e dentro dela chamar as loadviews para montar nossa página.
 Essa parte é bem parecida com aquela welcome que fizemos no início, so que em vez da view welcome vamos chamar contact.
 Por último eu criei a view contact, bem parecida com a welcome, só modifiquei o texto.
